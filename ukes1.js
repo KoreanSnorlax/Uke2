353beba4f31e6b7c73eabbeb2342a9c39d01a608
KoreanSnorlax <Mikael.sehwa.berg@gmail.com>
2021-03-17 13:00:47 +0100

    Create ukes1.js

---
+++ ukes1.js
@@ -0,0 +1,12 @@
+function bildeMikael(){
+    document.getElementById('mikael').innerHTML = `<img src="Bilder/mikael.jpg" class="bilde"/>`;
+}
+function bildeChris(){
+    document.getElementById('chris').innerHTML = `<img src="Bilder/chris.jpg" class="bilde"/>`;
+}
+function bildeMalene(){
+    document.getElementById('malene').innerHTML = `<img src="Bilder/malene.jpg" class="bilde"/>`;
+}
+function bildeAlexander(){
+    document.getElementById('alexander').innerHTML = `<img src="Bilder/alexander.jpg" class="bilde"/>`;
+}
